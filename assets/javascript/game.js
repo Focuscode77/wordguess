be2c531608c89b848234b9d6e075af4e18799b02
Joshua Coleman <procoleman77@outlook.com>
2019-08-20 17:27:56 -0400

    created a re-write js for game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,115 @@
 // Golabal Vaiables
 var urbanCitiesArr =["new york", "philadlephia", "dallas", "atlanta", "washington"];
-var wins = 0;
-var loss=0;
+var winsScore = 0;
+var lossScore=0;
 var wrongLetter=[];
 var guessesLeft=9;
 var underScores=[]
 var userGusses=[];
+
  var urbanCity;
- var winCounter = 0;
  
  
+ var urbanCity =  Math.floor(Math.random() * urbanCitiesArr.length);
+//  DOM 
+var wordblank = document.getElementById('word-blanks');
+var winsScore = document.getElementById('Wins');
+var lossScore = document.getElementById("Loses");
+ var choosenWord = urbanCitiesArr[urbanCity];
  
+  var createUnderscore = () =>{
+  for(var i=0; i < choosenWord.length; i++){
+      
+      underScores.push('_');
+      
+      
+  }
+     return underScores;
+
+  }
+ console.log(createUnderscore());
+document.addEventListener('keypress', (event) => {
+    var x = event.which || event.keyCode; 
+    var keyword = String.fromCharCode(event.keyCode);
+    // if users guess is right
+     if(choosenWord.indexOf(keyword) > -1){
+    // add to Users Gusses
+     userGusses.push(keyword);
+     underScores[choosenWord.indexOf(keyword)] = keyword;
+     wordblank[i].innerHTML = underScores.join('');
+      userGusses.innerHTML = winsScore;
+    
+      if (underScores.join('')=== choosenWord) {
+         alert("You win");
+         
+    }
+        
+     else{
+        wrongLetter.push(keyword);
+        console.log(wrongLetter);
+    }
+          
+      } 
+    
+
+});
+
 
  // Function to Start Game
- function startGame(){
-  urbanCity = urbanCitiesArr[Math.floor(Math.random() * urbanCitiesArr.length)];
+//  function startGame(){
 
-  for(var i=0; i < urbanCity.length; i++){
 
-      underScores.push('_');
 //   Print underscores to screen
-   document.getElementById('word-blanks').textContent =underScores.join(" ");
+    // document.getElementById('word-blanks').textContent =underScores.join(" ");
+    
 //    Reset 
-   wrongLetter=[];
-   guessesLeft=9;
+//    wrongLetter=[];
+//    guessesLeft=9;
+   
 
    //HTML Print
-   document.getElementById('guesses-left').textContent = guessesLeft;
+//    document.getElementById('guesses-left').textContent = guessesLeft;
 
- }
+//  }
  
-}
+// }
 // Win or lose function
-function winLose()
-{
-    if(winCounter === urbanCity.length){
+// function winLose()
+// {
+    // if(winCounter === urbanCity.length){
 
-        alert('winner');
-    }
-    else if (guessesLeft===0){
-        alert('Loser');
+        // alert('winner');
+    // }
+    // else if (guessesLeft===0){
+        // alert('Loser');
         
-}
-}
+// }
+// }
 
 // User Guesses
-document.onkeyup = function(event){
-userGusses = event.key;
+// document.onkeyup = function(event){
+// userGusses = event.key;
 
 
 // checking if letter exist inside of the word.
- if(urbanCity.indexOf(userGusses) > -1 ){
-     for( var i=0; i < urbanCity.length; i++){
-    if(urbanCity[i]===userGusses){
-        underScores[i]=userGusses;
-        console.log(underScores);
-        winCounter++;
-        winLose();
-    }
-     }
- }
+//  if(urbanCity.indexOf(userGusses) > -1 ){
+    //  for( var i=0; i < urbanCity.length; i++){
+    // if(urbanCity[i]===userGusses){
+        // underScores[i]=userGusses;
+        // console.log(underScores);
+        // winCounter++;
+        // winLose();
+    // }
+    //  }
+//  }
 
- else{
-     wrongLetter.push(userGusses);
-     guessesLeft--;
-     winLose();
+//  else{
+    //  wrongLetter.push(userGusses);
+    //  guessesLeft--;
+    //  winLose();
      
- }
-}
-startGame();
+     
+//  }
+// }
+// startGame();
 
-console.log(urbanCity);
